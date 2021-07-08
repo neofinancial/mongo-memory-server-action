98ded072d7b0052ab13d1c0e28e32bbb82cda9cc
Jeffrey Dugas <jeffrey.dugas@neofinancial.com>
2021-07-08 09:21:07 -0500

    Provide negative tests (i.e. expected failures)

--- test/factory.memory-server-factory.test.ts
+++ test/factory.memory-server-factory.test.ts
@@ -47,3 +47,67 @@ describe('MemoryServerFactory', () => {
     expect(server.instanceInfo?.instance.binaryOpts.version).toEqual(expectedVersion);
   });
 });
+
+describe('MemoryServerFactory with bad inputs', () => {
+  // suppress console.warn coming from mongo-memory-server
+  let consoleWarnSpy: jest.SpyInstance<void, [message?: unknown, ...optionalParams: unknown[]]>;
+
+  beforeAll(async () => {
+    consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation();
+  });
+
+  test('throws an error when provided an invalid version', async () => {
+    const badVersion = '9999';
+    const promise = MemoryServerFactory.generateMemoryServer(
+      expectedDbName,
+      expectedPort,
+      expectedStorageEngine,
+      badVersion
+    );
+
+    await expect(promise).rejects.toThrow("Status Code is 403 (MongoDB's 404)");
+  });
+
+  test('throws an error when provided an invalid storageEngine', async () => {
+    const badStorageEngine = 'noSuchEngine';
+    const promise = MemoryServerFactory.generateMemoryServer(
+      expectedDbName,
+      expectedPort,
+      badStorageEngine,
+      expectedVersion
+    );
+
+    await expect(promise).rejects.toThrow('Instance Exited before being ready and without throwing an error!');
+  });
+
+  test('throws an error when provided an invalid port', async () => {
+    const badPort = 999999;
+    const promise = MemoryServerFactory.generateMemoryServer(
+      expectedDbName,
+      badPort,
+      expectedStorageEngine,
+      expectedVersion
+    );
+
+    await expect(promise).rejects.toThrow(`options.port should be >= 0 and < 65536. Received ${badPort}.`);
+  });
+
+  test('unfortunately does NOT throw an error when provided a dbName that is too long', async () => {
+    const badDbName = 'monogram'.repeat(20);
+
+    return MemoryServerFactory.generateMemoryServer(
+      badDbName,
+      expectedPort,
+      expectedStorageEngine,
+      expectedVersion
+    ).then(_server => {
+      expect(_server).toBeInstanceOf(MongoMemoryServer);
+
+      return _server.stop(true)
+    })
+  });
+
+  afterAll(async () => {
+    consoleWarnSpy.mockRestore();
+  });
+});
